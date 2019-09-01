095e86c6a3ec616b65f72f02407db078f60320ec
Kelvin Almonte <KelvinAlmonte@live.com>
2019-08-31 20:36:30 -0400

    App js initialization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { MoonLoader } from "react-spinners";
+import classes from "./App.module.css";
+
+class App extends Component {
+  state = {
+    searchBarInput: "",
+    weatherDetails: {
+      temperature: "",
+      description: ""
+    },
+    loading: false,
+    error: false
+  };
+
+  searchBarHandler = e => {
+    this.setState({
+      searchBarInput: e.target.value
+    });
+  };
+
+  setWeather = () => {
+    console.log("Setting weather");
+    const city = this.state.searchBarInput;
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    this.setState({ weatherDetails: {}, loading: true, error: false }, () => {
+      // callback function
+    });
+  };
+
+  render() {
+    return (
+      <div className={classes.AppWrapper}>
+        <h1>Head Stuff</h1>
+        {/* <Header /> */}
+        <main className={classes.AppMain}>
+          <h6>Main Stuff</h6>
+          {/* <SearchBar />
+          <Card>
+            <WeatherDetails /> */}
+          {/* <MoonLoader /> */}
+          {/* <Preview /> */}
+          {/* <ErrorNotice /> */}
+          {/* </Card> */}
+        </main>
+        {/* <Footer /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
