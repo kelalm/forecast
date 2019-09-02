204535125133a7f83b17b65893c98383420b0bbd
Kelvin Almonte <KelvinAlmonte@live.com>
2019-09-02 02:20:03 -0400

    Added fixes to weather representation

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 
+import { ReactComponent as DefaultImage } from "../assets/images/Preview.svg";
+import { ReactComponent as Clear } from "../assets/images/Clear.svg";
+
 export default class WeatherDetails extends Component {
   render() {
+    var conditionsText = this.props.conditions;
+    conditionsText = conditionsText
+      .toLowerCase()
+      .split(" ")
+      .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+      .join(" ");
+
+    let conditionImage = <DefaultImage />;
+
+    if (conditionsText === "Clear Sky") {
+      conditionImage = <Clear />;
+    }
     return (
       <div>
         <h4>{this.props.city}</h4>
         <h1>{Math.round(this.props.temperature)}°F</h1>
-        <h2>{this.props.conditions}</h2>
+        <h2>{conditionsText}</h2>
+        {conditionImage}
       </div>
     );
   }
