c55f95681942953db8f919625f1b827d5941959f
Kelvin Almonte <KelvinAlmonte@live.com>
2019-09-04 19:52:25 -0400

    Updates

---
+++ precache-manifest.b5e8477da90571ef19510c0fdda48800.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bcbf3a5ae659cddd4f7a78ccca26732",
+    "url": "/forecast/index.html"
+  },
+  {
+    "revision": "5ee196c97ed514cf1980",
+    "url": "/forecast/static/css/main.0ce1a807.chunk.css"
+  },
+  {
+    "revision": "71afacfeaf81351c2a6a",
+    "url": "/forecast/static/js/2.b1f23091.chunk.js"
+  },
+  {
+    "revision": "5ee196c97ed514cf1980",
+    "url": "/forecast/static/js/main.ef0e7a1d.chunk.js"
+  },
+  {
+    "revision": "5520306a93e24ce06fb1",
+    "url": "/forecast/static/js/runtime~main.a4a3ed72.js"
+  },
+  {
+    "revision": "f963a166b3279d455437165cb78cce6a",
+    "url": "/forecast/static/media/Clear.f963a166.svg"
+  },
+  {
+    "revision": "a98369fb7ac4ac2d522c92642bb69bc8",
+    "url": "/forecast/static/media/Clouds.a98369fb.svg"
+  },
+  {
+    "revision": "29a0aca0c7b11b9e2e20e203bca66089",
+    "url": "/forecast/static/media/Preview.29a0aca0.svg"
+  },
+  {
+    "revision": "b5e8855f869aeaa037290f87cf347fd8",
+    "url": "/forecast/static/media/Rain.b5e8855f.svg"
+  }
+]);
