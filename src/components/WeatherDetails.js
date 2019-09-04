d65512a9313b8c76a9ec3e1e3337305a12ae1612
Kelvin Almonte <KelvinAlmonte@live.com>
2019-09-04 19:47:58 -0400

    Added more images for different conditions

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 import { ReactComponent as DefaultImage } from "../assets/images/Preview.svg";
 import { ReactComponent as Clear } from "../assets/images/Clear.svg";
+import { ReactComponent as Rain } from "../assets/images/Rain.svg";
+import { ReactComponent as Clouds } from "../assets/images/Clouds.svg";
 
 export default class WeatherDetails extends Component {
   render() {
@@ -14,9 +16,14 @@ export default class WeatherDetails extends Component {
 
     let conditionImage = <DefaultImage />;
 
-    if (conditionsText === "Clear Sky") {
+    if (conditionsText.toLowerCase().includes("clear")) {
       conditionImage = <Clear />;
+    } else if (conditionsText.toLowerCase().includes("rain")) {
+      conditionImage = <Rain />;
+    } else if (conditionsText.toLowerCase().includes("cloud")) {
+      conditionImage = <Clouds />;
     }
+
     return (
       <div className="infoLabels">
         <p className="cityLabel">{this.props.city}</p>
